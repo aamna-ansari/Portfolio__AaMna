c1de744baa46b56964b41d85446d917a25e7ac8a
aamna-ansari <theamnabb@gmail.com>
2025-05-31 20:51:24 +0500

    erro fix

--- src/utility/sendMail.ts
+++ src/utility/sendMail.ts
@@ -12,6 +12,8 @@ export const sendMail = async function (
   
   const user = process.env.NODEMAILER_USER;
   const pass = process.env.NODEMAILER_PASS;
+  console.log("Sending mail to:", user);
+  console.log("Email from:", email);
   if (!user || !pass) {
     return new Promise((resolve) =>
       resolve({ status: 500, message: "Internal server error" }),
